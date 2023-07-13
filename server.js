ee0180f22aa4d3bc2d4a679a68af9f84005fc161
Youness <oubelkacem.youness@gmail.com>
2023-07-13 22:00:20 +0100

    second commit

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const scraper = require('./controllers/scraper')
+const scraper = require("./controllers/scraper");
 
-const port = 3000
+const port = 3000;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
 
-app.get('/', async (req, res)=>{
-    
-    res.render('index', {data: await scraper()})
-})
+app.use(express.static("public"));
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.get("/", async (req, res) => {
+  res.render("index", { data: await scraper() });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
